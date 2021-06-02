ffb740439f781e1b568031beaeea44fe90552ea2
BrianBTI <bertili.brian@gmail.com>
2021-06-02 14:00:08 +0200

    cleaned app.jsx and created a NavBar.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  NavLink,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Create from "./components/Create.jsx";
-import Home from "./components/Home.jsx";
-import Library from "./components/Library.jsx";
-import Profile from "./components/Profile.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
-  const linkTo = (path, componentName) => (
-    <NavLink activeClassName="current" className="link" exact to={path}>
-      {componentName}
-    </NavLink>
-  );
-
-  const routeTo = (path, componentDisplayed) => (
-    <Route exact path={path} component={componentDisplayed} />
-  );
-
   return (
     <>
-      <Router>
-        {linkTo("/", "Accueil")};{linkTo("/profile", "Profile")};
-        {linkTo("/create", "Créer")};{linkTo("/library", "Bibliothèque")};
-        <Switch>
-          {routeTo("/", Home)};{routeTo("/profile", Profile)};
-          {routeTo("/create", Create)};{routeTo("/library", Library)};
-        </Switch>
-      </Router>
-      <div className="App">
-        <p>Hello World !</p>
-      </div>
+      <NavBar />
     </>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Create from "./Create.jsx";
+import Home from "./Home.jsx";
+import Library from "./Library.jsx";
+import Profile from "./Profile.jsx";
+
+import "../CSS/NavBar.css";
+
+function NavBar() {
+  const linkTo = (path, componentName) => (
+    <NavLink activeClassName="current" className="link" exact to={path}>
+      {componentName}
+    </NavLink>
+  );
+
+  const routeTo = (path, componentDisplayed) => (
+    <Route exact path={path} component={componentDisplayed} />
+  );
+
+  return (
+    <>
+      <Router>
+        <ul>
+          {linkTo("/", "Accueil")} {linkTo("/profile", "Profile")}
+          {linkTo("/create", "Créer")} {linkTo("/library", "Bibliothèque")}
+        </ul>
+        <Switch>
+          {routeTo("/", Home)};{routeTo("/profile", Profile)};
+          {routeTo("/create", Create)};{routeTo("/library", Library)};
+        </Switch>
+      </Router>
+      <div className="App">
+        <p>Hello World !</p>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
