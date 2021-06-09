97cadf239e58f434e4baa736b6d611581fcc7cb0
denis-webpro <denis.webpro@gmail.com>
2021-06-09 12:23:30 +0200

    1) Uninstall Tailwind 2) Install Styled-Components 3) Create component RecentlyCreated

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import RecentlyCreated from "../components/RecentlyCreated.jsx";
+
+const Container = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: repeat(3, 1fr);
+`;
+
+const LeftColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-column: 2;
+  justify-content: flex-start;
+  border-left: solid 3px black;
+  height: 60%;
+`;
 
 function Create() {
   return (
     <div>
-      <h1>Hello from Create !</h1>
+      <h1>STUDIO PIPELETTE</h1>
+      <h2>Enregistrez votre capsule sonore</h2>
+      <Container>
+        <RecentlyCreated />
+      </Container>
+      <LeftColumn>
+        <h1>1</h1>
+      </LeftColumn>
     </div>
   );
 }
