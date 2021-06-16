383f96cf7c7c2acacba0f937f1764f9125ed93d0
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-06-16 13:40:09 +0200

    Added carroussel to the videos, the videos are just tests and they will chance soon.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import "../CSS/Home.css";
 import Pipelette from "../video/Pipelette.mp4";
+import Test from "../video/Test.mp4";
+import Test2 from "../video/Test2.mp4";
+
+const videos = [Pipelette, Test, Test2];
 
 function Home() {
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   return (
     <div>
       <h1>Pour dire Je T&apos;aime, Merci, Félicitations.</h1>
@@ -11,8 +16,20 @@ function Home() {
         className="VideoPlayer"
         controls
         width="100vw"
-        url={Pipelette}
+        url={videos[currentVideoIndex]}
       />
+      <button
+        className="Previous"
+        type="button"
+        onClick={() => setCurrentVideoIndex(currentVideoIndex - 1)}
+      ></button>
+      <button
+        className="Next"
+        type="button"
+        onClick={() => setCurrentVideoIndex(currentVideoIndex + 1)}
+      >
+        Next
+      </button>
     </div>
   );
 }
