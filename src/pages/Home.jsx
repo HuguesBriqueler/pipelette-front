3db98ed85f5b7e906fc3fca6b79e5af4748db92b
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-06-16 14:52:01 +0200

    Fixed Home CSS and added lorem text for a future field

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,23 @@ import Pipelette from "../video/Pipelette.mp4";
 function Home() {
   return (
     <div>
-      <h1>Pour dire Je T&apos;aime, Merci, Félicitations.</h1>
-      <ReactPlayer
-        className="VideoPlayer"
-        controls
-        width="100vw"
-        url={Pipelette}
-      />
+      <h1 className="title">Pipelette</h1>
+      <h2 className="slogan">
+        Pour dire Je T&apos;aime, Merci, Félicitations.
+      </h2>
+      <div className="VideoPlayer">
+        <ReactPlayer controls width="100%" url={Pipelette} />
+      </div>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere at quam
+        reprehenderit possimus nisi, voluptate non enim ratione alias
+        architecto, molestiae deleniti amet voluptates libero odio soluta
+        maiores, dolor quod? Quod facilis iste id ex asperiores iusto dicta
+        animi fugiat ipsam dolorum. Assumenda explicabo deserunt obcaecati iste,
+        praesentium maiores nulla ut, nam quod, quidem inventore dolor
+        accusantium! Iusto natus error libero{" "}
+      </p>
     </div>
   );
 }
