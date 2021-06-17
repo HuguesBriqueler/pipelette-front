0deb7e5c03c1d458174cf2cded9377b6922ce044
BrianBTI <bertili.brian@gmail.com>
2021-06-17 16:51:21 +0200

    Cleaned code by creating a new component: IsNotLogged

---
+++ src/pages/IsNotLogged.jsx
@@ -0,0 +1,62 @@
+import React, { useRef } from "react";
+import { useAuthentication } from "../contexts/AuthenticationContext.jsx";
+
+function IsNotLogged() {
+  const emailInput = useRef();
+  const passwordInput = useRef();
+  const { setAuthentication } = useAuthentication();
+
+  return (
+    <>
+      <h1>Bienvenue sur la page de connexion !</h1>
+      <form
+        className="wrapper"
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          const user = {
+            email: emailInput.current.value,
+            password: passwordInput.current.value,
+          };
+
+          console.log(user);
+
+          const config = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+          };
+          const url = "http://localhost:5000/users/login";
+          fetch(url, config)
+            .then((res) => res.json())
+            .then((data) => {
+              const { token } = data;
+              console.log(token);
+              setAuthentication(token);
+            });
+        }}
+      >
+        <div className="email">
+          <label htmlFor="email" placeholder="youremail@gmail.com">
+            Email:{" "}
+          </label>
+          <input ref={emailInput} type="email" name="email" id="email" />
+        </div>
+        <div className="password">
+          <label htmlFor="password">Mot de passe: </label>
+          <input
+            ref={passwordInput}
+            type="password"
+            name="password"
+            id="password"
+          />
+        </div>
+        <button>Valider</button>
+      </form>
+    </>
+  );
+}
+
+export default IsNotLogged;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,9 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useAuthentication } from "../contexts/AuthenticationContext.jsx";
+import IsNotLogged from "./IsNotLogged.jsx";
 
 function Login() {
-  const emailInput = useRef();
-  const passwordInput = useRef();
-
-  const { authentication, setAuthentication } = useAuthentication();
+  const { authentication } = useAuthentication();
 
   if (authentication != null) {
     return (
@@ -16,57 +14,7 @@ function Login() {
     );
   }
 
-  return (
-    <>
-      <h1>Bienvenue sur la page de connexion !</h1>
-      <form
-        className="wrapper"
-        onSubmit={(event) => {
-          event.preventDefault();
-
-          const user = {
-            email: emailInput.current.value,
-            password: passwordInput.current.value,
-          };
-
-          console.log(user);
-
-          const config = {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(user),
-          };
-          const url = "http://localhost:5000/users/login";
-          fetch(url, config)
-            .then((res) => res.json())
-            .then((data) => {
-              const { token } = data;
-              console.log(token);
-              setAuthentication(token);
-            });
-        }}
-      >
-        <div className="email">
-          <label htmlFor="email" placeholder="youremail@gmail.com">
-            Email:{" "}
-          </label>
-          <input ref={emailInput} type="email" name="email" id="email" />
-        </div>
-        <div className="password">
-          <label htmlFor="password">Mot de passe: </label>
-          <input
-            ref={passwordInput}
-            type="password"
-            name="password"
-            id="password"
-          />
-        </div>
-        <button>Valider</button>
-      </form>
-    </>
-  );
+  return <IsNotLogged />;
 }
 
 export default Login;
