eefaf49c6f359112400b22a39034769def837708
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-06-17 14:03:20 +0200

    Implemented Audio Recorder into the project

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,79 +1,113 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
+import AudioReactRecorder, { RecordState } from "audio-react-recorder";
 import "../CSS/RecentCapsules.css";
+import "../CSS/Create.css";
 
-const Section = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+// const Section = styled.section`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
 
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  height: 50vh;
-`;
+// const Container = styled.div`
+//   display: grid;
+//   grid-template-columns: 1fr 1fr;
+//   height: 50vh;
+// `;
 
-const ColumnLeft = styled.div`
-  display: flex;
-  row-gap: 10px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+// const ColumnLeft = styled.div`
+//   display: flex;
+//   row-gap: 10px;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
 
-  h1 {
-    margin-bottom: 0.5rem;
-    font-size: 1.2rem;
+//   h1 {
+//     margin-bottom: 0.5rem;
+//     font-size: 1.2rem;
+//   }
+// `;
+
+// const ColumnRight = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+class Create extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recordState: null,
+      audioData: null,
+    };
   }
-`;
 
-const ColumnRight = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
+  start = () => {
+    this.setState({
+      recordState: RecordState.START,
+    });
+  };
 
-function Create() {
-  return (
-    <div>
-      <h1 className="titre1">STUDIO PIPELETTE</h1>
-      <h2 className="titre2">Enregistrez votre capsule sonore</h2>
-      <div className="fakeImage">
-        <h1>Image</h1>
+  pause = () => {
+    this.setState({
+      recordState: RecordState.PAUSE,
+    });
+  };
+
+  stop = () => {
+    this.setState({
+      recordState: RecordState.STOP,
+    });
+  };
+
+  onStop = (data) => {
+    this.setState({
+      audioData: data,
+    });
+    console.log("onStop: audio data", data);
+  };
+
+  render() {
+    const { recordState } = this.state;
+
+    return (
+      <div>
+        <AudioReactRecorder
+          state={recordState}
+          onStop={this.onStop}
+          backgroundColor="rgb(255,255,255)"
+          foregroundColor="#f5bbb7"
+          className="audiorecord"
+        />
+        <audio
+          id="audio"
+          controls
+          src={this.state.audioData ? this.state.audioData.url : null}
+        ></audio>
+        <div className="recordButtonsContainer">
+          <button className="recordButtons" id="record" onClick={this.start}>
+            <span role="img" aria-label="play">
+              &#128308;
+            </span>
+          </button>
+          <button className="recordButtons" id="pause" onClick={this.pause}>
+            <span role="img" aria-label="pause">
+              &#9612;&#9612;
+            </span>
+          </button>
+          <button className="recordButtons" id="stop" onClick={this.stop}>
+            <span role="img" aria-label="stop">
+              &#9607;
+            </span>
+          </button>
+        </div>
       </div>
-      <Section>
-        <Container>
-          <ColumnLeft>
-            <div>
-              <p className="logo">👧</p>
-            </div>
-            <div className="capsules">
-              <h1>Capsule1</h1>
-            </div>
-            <div className="capsules">
-              <h1>Capsule2</h1>
-            </div>
-
-            <div className="capsules">
-              <h1>Capsule3</h1>
-            </div>
-          </ColumnLeft>
-          <ColumnRight>
-            <p className="texts">
-              Le Lorem Ipsum est simplement du faux texte employé dans la
-              composition et la mise en page avant impression.
-            </p>
-            <p className="symbols">🎙</p>
-            <p className="texts">Enregistrer</p>
-            <p className="symbols">⬆ 📂</p>
-            <p className="symbols"></p>
-            <p className="texts">Charger</p>
-          </ColumnRight>
-        </Container>
-      </Section>
-    </div>
-  );
+    );
+  }
 }
 
 export default Create;
