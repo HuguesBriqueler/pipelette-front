25f5ad7b615f2e841e257b6a1d06e4f4d55f7fa6
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-06-17 15:55:57 +0200

    fixed issues

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,40 +1,7 @@
 import React from "react";
-// import styled from "styled-components";
 import AudioReactRecorder, { RecordState } from "audio-react-recorder";
 import "../CSS/RecentCapsules.css";
 import "../CSS/Create.css";
-
-// const Section = styled.section`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// const Container = styled.div`
-//   display: grid;
-//   grid-template-columns: 1fr 1fr;
-//   height: 50vh;
-// `;
-
-// const ColumnLeft = styled.div`
-//   display: flex;
-//   row-gap: 10px;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-
-//   h1 {
-//     margin-bottom: 0.5rem;
-//     font-size: 1.2rem;
-//   }
-// `;
-
-// const ColumnRight = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-// `;
 
 class Create extends React.Component {
   constructor(props) {
@@ -76,35 +43,9 @@ class Create extends React.Component {
 
     return (
       <div>
-        <AudioReactRecorder
-          state={recordState}
-          onStop={this.onStop}
-          backgroundColor="rgb(255,255,255)"
-          foregroundColor="#f5bbb7"
-          className="audiorecord"
-        />
-        <audio
-          id="audio"
-          controls
-          src={this.state.audioData ? this.state.audioData.url : null}
-        ></audio>
-        <div className="recordButtonsContainer">
-          <button className="recordButtons" id="record" onClick={this.start}>
-            <span role="img" aria-label="play">
-              &#128308;
-            </span>
-          </button>
-          <button className="recordButtons" id="pause" onClick={this.pause}>
-            <span role="img" aria-label="pause">
-              &#9612;&#9612;
-            </span>
-          </button>
-          <button className="recordButtons" id="stop" onClick={this.stop}>
-            <span role="img" aria-label="stop">
-              &#9607;
-            </span>
-          </button>
-        </div>
+        <h1>Studio Pipelette</h1>
+        <h2 id="audioInstructions">Enregitrez votre capsule sonore</h2>
+        <button type="button"></button>
       </div>
     );
   }
