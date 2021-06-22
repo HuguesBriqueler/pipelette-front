08e1e025910ab97cbd852bd2812e9105ce592e4b
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-06-22 16:11:47 +0200

    Fixed the button, it now sets the state to true instead of just toggling true/false and once clicked it shows the voice recorder component

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -11,7 +11,6 @@ class Create extends React.Component {
       recordState: null,
       audioData: null,
       isRecording: false,
-      microIsDisplayed: true,
     };
   }
 
@@ -45,7 +44,7 @@ class Create extends React.Component {
 
     const startRecording = () => {
       this.setState({
-        isRecording: !this.state.isRecording,
+        isRecording: true,
       });
     };
 
@@ -53,20 +52,22 @@ class Create extends React.Component {
       <div className="microphone">
         <h1>Studio Pipelette</h1>
         <h2 id="audioInstructions">Enregitrez votre capsule sonore</h2>
-        <div>
-          <section className="portfolio-experiment">
-            <a type="button" id="microphone" onClick={() => startRecording()}>
-              <img
-                id="audioInstructions"
-                src="https://img.icons8.com/wired/64/000000/microphone.png"
-              />
-              <span className="line -right"></span>
-              <span className="line -top"></span>
-              <span className="line -left"></span>
-              <span className="line -bottom"></span>
-            </a>
-          </section>
-        </div>
+        {!this.state.isRecording && (
+          <div>
+            <section className="portfolio-experiment">
+              <a type="button" id="microphone" onClick={() => startRecording()}>
+                <img
+                  id="audioInstructions"
+                  src="https://img.icons8.com/wired/64/000000/microphone.png"
+                />
+                <span className="line -right"></span>
+                <span className="line -top"></span>
+                <span className="line -left"></span>
+                <span className="line -bottom"></span>
+              </a>
+            </section>
+          </div>
+        )}
         {this.state.isRecording && (
           <div>
             <AudioReactRecorder
