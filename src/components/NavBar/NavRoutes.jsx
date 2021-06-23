cb235ba5eb4c0ca7c0762eaf897be27a90f5bf79
BrianBTI <bertili.brian@gmail.com>
2021-06-23 09:59:49 +0200

    Changed what was asked on PR NavBarIfLogged in NavRoutes, created a an intermediate component for /library route

--- src/components/NavBar/NavRoutes.jsx
+++ src/components/NavBar/NavRoutes.jsx
@@ -1,29 +1,25 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import Create from "../../pages/Studio.jsx";
 import Home from "../../pages/Home.jsx";
-import Playlist from "../../pages/Playlist.jsx";
 import Profile from "../../pages/Profile.jsx";
 import Register from "../../pages/Register.jsx";
 import Login from "../../pages/Login.jsx";
 import Logout from "../Logout.jsx";
-import { useAuthentication } from "../../contexts/AuthenticationContext.jsx";
+import PlaylistIfLogged from "../PlaylistIfLogged.jsx";
 
 function NavRoutes() {
   const routeTo = (path, componentDisplayed) => (
     <Route exact path={path} component={componentDisplayed} />
   );
 
-  const { authentication } = useAuthentication;
   return (
     <main>
       {routeTo("/", Home)}
       {routeTo("/profile", Profile)}
       {routeTo("/create", Create)}
-      <Route exact path="/library">
-        {authentication ? { Playlist } : <Redirect to="/login" />}
-      </Route>
+      {routeTo("/library", PlaylistIfLogged)}
       {routeTo("/logout", Logout)}
       {routeTo("/register", Register)}
       {routeTo("/login", Login)}
