5a5eaf62c6f6cc6a32bc066084e3800bc5242a3b
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-07-06 10:33:35 +0200

    fixed required changes

--- src/pages/Studio.jsx
+++ src/pages/Studio.jsx
@@ -44,7 +44,7 @@ class Create extends React.Component {
         console.log(blob);
         const formData = new FormData();
         formData.append("blob", blob);
-        const url = "http://localhost:5000/toto";
+        const url = "http://localhost:5000/capsule_upload";
         fetch(url, {
           method: "post",
           body: formData,
@@ -117,13 +117,6 @@ class Create extends React.Component {
                 &#9607;
               </button>
             </div>
-            {/* <button
-              id="saveButton"
-              type="button"
-              onClick={() => saveRecording()}
-            >
-              Sauvegarder
-            </button> */}
           </>
         ) : (
           <section className="portfolio-experiment">
